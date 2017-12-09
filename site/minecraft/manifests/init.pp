88bb81adba3f78bd2dc23d22be76c4d497aaae23
tylerecouture <tylerecouture@users.noreply.github.com>
2017-12-08 20:33:43 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
   }
   file {'/opt/minecraft/minecraft_server.jar':
     ensure => file
-    src    => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   package {'java':
     ensure => present,
@@ -15,7 +15,7 @@ class minecraft {
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    src    => ''puppet:///modules/minecraft/minecraft.service',
+    source => ''puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
    ensure => running,
