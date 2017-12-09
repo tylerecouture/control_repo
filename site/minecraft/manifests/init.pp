c7ac5890b08c50b1cf4f9b6d7db3afd28668109b
tylerecouture <tylerecouture@users.noreply.github.com>
2017-12-08 20:55:42 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,15 +1,19 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar
+  $install_dir = '/opt/minecraft'
+
+  file {$install_dir':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {"{$install_dir}/minecraft_server.jar":
     ensure => file,
-    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => $url,
+    before => Service['minecraft],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"{$install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
   }
@@ -20,5 +24,6 @@ class minecraft {
   service {'minecraft':
    ensure => running,
    enable => true,
+   require => [Package['java'],File["{$install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']]
   }
 }
