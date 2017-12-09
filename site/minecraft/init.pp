2199160a421574e3d295434c509639f773dee392
tylerecouture <tylerecouture@users.noreply.github.com>
2017-12-08 20:25:29 -0800

    Create init.pp

---
+++ site/minecraft/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+  file {'/opt/minecraft';
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar';
+    ensure => file
+    src    => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure  => file,
+    content => 'eual=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    src    => ''puppet:///modules/minecraft/minecraft.service',
+  }
+}
