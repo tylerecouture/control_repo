f8bdf25adb71fce38fe7661e2815e1d7ccf36060
tylerecouture <tylerecouture@users.noreply.github.com>
2017-12-08 20:34:29 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,7 +3,7 @@ class minecraft {
     ensure => directory,
   }
   file {'/opt/minecraft/minecraft_server.jar':
-    ensure => file
+    ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   package {'java':
